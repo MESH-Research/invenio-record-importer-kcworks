8eb3b4bd112fd79986e0fc9899631070f1946cb4
Ian Scott <scottianw@gmail.com>
2024-11-07 10:34:17 -0500

    fix: Fixed handling of no-updates flag

--- invenio_record_importer_kcworks/record_loader.py
+++ invenio_record_importer_kcworks/record_loader.py
@@ -1211,6 +1211,7 @@ def load_records_into_invenio(
     failed_records = []
     created_records = []
     skipped_records = []
+    no_updates_records = []
     successful_records = 0
     updated_drafts = 0
     updated_published = 0
@@ -1496,6 +1497,8 @@ def load_records_into_invenio(
                             **log_object, skipped_records=skipped_records
                         )
                     )
+                elif e.__class__.__name__ == "NoUpdates":
+                    no_updates_records.append(log_object)
                 if stop_on_error and failed_records:
                     break
 
@@ -1519,14 +1522,16 @@ def load_records_into_invenio(
         f"Processed {str(record_counter)} records in InvenioRDM ({set_string})"
         f" \n    {str(successful_records)} successful \n   "
         f" {str(new_records)} new records created \n   "
-        f" {str(successful_records - new_records)} already existed \n       "
+        f" {str(successful_records - new_records + len(no_updates_records))} already existed \n       "
         f" {str(updated_published)} updated published records \n       "
         f" {str(updated_drafts)} updated existing draft records \n       "
-        f" {str(unchanged_existing)} unchanged existing records \n       "
+        f" {str(unchanged_existing + len(no_updates_records))} unchanged existing records \n       "
         f" {str(len(repaired_failed))} previously failed records repaired \n "
         f"   {str(len(failed_records))} failed \n"
         f"   {str(len(skipped_records))} records skipped (marked in overrides)"
         f"\n   "
+        f"   {str(len(no_updates_records))} records not updated because "
+        f"'no updates' flag was set \n   "
     )
     app.logger.info(message)
 
