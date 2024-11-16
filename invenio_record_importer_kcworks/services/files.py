131c43f39063a7ff7c74649c8a57fdfe7992b3ed
Ian Scott <scottianw@gmail.com>
2024-11-15 19:42:57 -0500

    fix: Fixed handling of metadata only records

--- invenio_record_importer_kcworks/services/files.py
+++ invenio_record_importer_kcworks/services/files.py
@@ -574,73 +574,79 @@ class FilesHelper:
             (existing_draft_files, records_service.draft_files),
             (existing_published_files, records_service.files),
         ]:
-            print("files service:", files_service.config)
-            print("existing files:", existing_files)
-            print("new entries:", new_entries)
-            if len(existing_files) == 0 or old_files == {}:
-                if len(new_entries) > 0:
-                    same_files = False
+            if existing_files.length > 0:
+                print("files service:", files_service.config)
+                print("existing files:", existing_files)
+                print("new entries:", new_entries)
+                if len(existing_files) == 0 or old_files == {}:
+                    if len(new_entries) > 0:
+                        same_files = False
 
-                record = files_service._get_record(
-                    draft_id, system_identity, "delete_files"
-                )
-                print("record.files.entries:", record.files.entries)
-                if record.files.entries:
-                    record.files.unlock()
-                    record.files.delete_all(
-                        remove_obj=True, softdelete_obj=False, remove_rf=True
+                    record = files_service._get_record(
+                        draft_id, system_identity, "delete_files"
                     )
-                    app.logger.info(
-                        "    deleted all files from existing record"
-                    )
+                    print("record.files.entries:", record.files.entries)
+                    if record.files.entries:
+                        record.files.unlock()
+                        record.files.delete_all(
+                            remove_obj=True,
+                            softdelete_obj=False,
+                            remove_rf=True,
+                        )
+                        app.logger.info(
+                            "    deleted all files from existing record"
+                        )
+                    else:
+                        app.logger.info(
+                            "    no files attached to existing record"
+                        )
                 else:
-                    app.logger.info("    no files attached to existing record")
-            else:
-                normalized_new_keys = [
-                    unicodedata.normalize("NFC", k) for k in new_entries.keys()
-                ]
-                print("normalized new keys:", normalized_new_keys)
-                old_wrong_files = [
-                    f
-                    for f in existing_files
-                    if unicodedata.normalize("NFC", f["key"])
-                    not in normalized_new_keys
-                ]
-                print("old_wrong_files:", old_wrong_files)
-                for o in old_wrong_files:
-                    print("old wrong file:", o)
-                    self._delete_file(
-                        draft_id,
-                        o["key"],
-                        files_service=files_service,
-                    )
-
-                for k, v in new_entries.items():
-                    wrong_file = False
-                    existing_file = [
+                    normalized_new_keys = [
+                        unicodedata.normalize("NFC", k)
+                        for k in new_entries.keys()
+                    ]
+                    print("normalized new keys:", normalized_new_keys)
+                    old_wrong_files = [
                         f
                         for f in existing_files
                         if unicodedata.normalize("NFC", f["key"])
-                        == unicodedata.normalize("NFC", k)
+                        not in normalized_new_keys
                     ]
-
-                    if len(existing_file) == 0:
-                        same_files = False
-                        print("no existing file found")
-
-                    elif (existing_file[0]["status"] == "pending") or (
-                        str(v["size"]) != str(existing_file[0]["size"])
-                    ):
-                        same_files = False
-                        wrong_file = True
-                        print("pending or size mismatch")
-
-                    if wrong_file:
+                    print("old_wrong_files:", old_wrong_files)
+                    for o in old_wrong_files:
+                        print("old wrong file:", o)
                         self._delete_file(
                             draft_id,
-                            existing_file[0]["key"],
+                            o["key"],
                             files_service=files_service,
                         )
+
+                    for k, v in new_entries.items():
+                        wrong_file = False
+                        existing_file = [
+                            f
+                            for f in existing_files
+                            if unicodedata.normalize("NFC", f["key"])
+                            == unicodedata.normalize("NFC", k)
+                        ]
+
+                        if len(existing_file) == 0:
+                            same_files = False
+                            print("no existing file found")
+
+                        elif (existing_file[0]["status"] == "pending") or (
+                            str(v["size"]) != str(existing_file[0]["size"])
+                        ):
+                            same_files = False
+                            wrong_file = True
+                            print("pending or size mismatch")
+
+                        if wrong_file:
+                            self._delete_file(
+                                draft_id,
+                                existing_file[0]["key"],
+                                files_service=files_service,
+                            )
 
             print("same files:", same_files)
             return same_files
